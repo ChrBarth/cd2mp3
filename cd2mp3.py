4840ae935fdff4b736b06691edf23f1d463c5963
ChrBarth <ChrBarth@users.noreply.github.com>
2017-10-29 18:59:30 +0100

    command line options now working

--- cd2mp3.py
+++ cd2mp3.py
@@ -4,7 +4,8 @@
 # rips audio cd to mp3-files into current directory
 
 #TODO:
-# * parse commandline arguments
+# get year from musicbrainz
+# maybe do the tagging while encoding with lame (so we would not need id3v2 any more)
 
 # requires discid and musicbrainzngs for musicbrainz-support (instead of cddb)
 # (pip3 install discid musicbrainzngs)
@@ -17,13 +18,13 @@ import argparse
 
 device      = '/dev/cdrom'
 bitrate     = 256
-use_cddb    = False
 encoder     = 'lame'
 do_encode   = True
-do_rip      = False
-remove_wav  = False
+do_rip      = True
+remove_wav  = True
 write_tags  = True
 directory   = os.getcwd()
+cur_dir     = directory
 
 artist   = 'unknown'
 title    = 'unknown'
@@ -37,11 +38,41 @@ parser = argparse.ArgumentParser(description="Rips audio cd to mp3 files")
 parser.add_argument("-b", "--bitrate", type=int, help="specify bitrate")
 parser.add_argument("-d", "--device", help="path to your cdrom-device")
 parser.add_argument("-D", "--workdir", help="output directory")
+parser.add_argument("--norip", action="store_true", help="Don't rip the cd (just encode wav-files)")
+parser.add_argument("--notags", action="store_true", help="Don't write id3-tags")
+parser.add_argument("--nodelete", action="store_true", help="Don't delete wav files after encoding")
+parser.add_argument("-y", "--year", help="id3-tag: year")
+parser.add_argument("-a", "--artist", help="id3-tag: artist")
+parser.add_argument("-A", "--album", help="id3-tag: album title")
+parser.add_argument("-g", "--genre", type=int, help="id3-tag: genre (integer) - see id3v2 -L")
+parser.add_argument("-e", "--encoder", help="use other encoder than lame")
+args = parser.parse_args()
 
-parser.parse_args()
-
+if args.bitrate != None:
+    bitrate = args.bitrate
+if args.device != None:
+    device = args.device
+if args.workdir != None:
+    directory = args.workdir
+if args.norip:
+    do_rip = False
+if args.notags:
+    write_tags = False
+if args.nodelete:
+    remove_wav = False
+if args.year != None:
+    year = args.year
+if args.artist != None:
+    artist = args.artist
+if args.album != None:
+    title = args.album
+if args.genre != None:
+    genre = args.genre
+if args.encoder != None:
+    encoder = args.encoder
+    
 # changing to working directory:
-if os.isdir(directory):
+if os.path.isdir(directory):
     os.chdir(directory)
 
 # get discid:
@@ -102,11 +133,12 @@ if do_encode == True:
         if write_tags == True:
             print("Writing id3v2-tag for %s (%s)..." % (mp3file, track[trackno]))
             tagproc = subprocess.run(["id3v2", "-a", artist, "-A", title,
-                                      "-t", track[trackno], mp3file],
+                                      "-t", track[trackno], "-y", str(year),
+                                      "-T", str(trackno+1), "-g", str(genre), mp3file],
                                       stdout=subprocess.PIPE)
         if remove_wav == True:
             # remove .wav file:
             os.remove(wavfile)
         trackno = trackno+1
 
-
+os.chdir(cur_dir)
